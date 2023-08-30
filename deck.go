53aca6e15403c08d0776b5caf8cc4b310c869a98
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-08-30 09:59:48 -0500

    instance method added.

--- deck.go
+++ deck.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 //create a new type of deck
 //which is a slice of strings
 type deck []string
+
+//create function for instances to use as method. function has a receiver.
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{newCard(), newCard(), "Ace of Diamonds"}
+	cards := deck{newCard(), newCard(), "Ace of Diamonds"}
 	cards = append(cards, "New Element")
 	//iterate over slice
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
