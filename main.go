086920c722481dcc6a17532fb00a2b055bbd7f09
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-08-30 09:46:42 -0500

    updated

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{newCard(), newCard(), "Ace of Diamonds"}
+	cards = append(cards, "New Element")
+	//iterate over slice
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
