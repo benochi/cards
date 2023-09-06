c9b5f17427a05c9ea44956fc686566517632f05a
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-09-06 09:05:00 -0500

    more testing

--- deck_test.go
+++ deck_test.go
@@ -1,8 +1,11 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
-//notice case
+// notice case
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	if len(d) != 52 {
@@ -15,4 +18,20 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King of Clubs" {
 		t.Errorf("Expected last card of King of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+// if opening a file make sure to handle cleanup in case of crash.
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting") //remove in case of crashes
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting") //remove after testing for cleanup.
 }
